908bcb077263b1793ac694938de8737c889c018d
Mike Dixon <qualitydixon@gmail.com>
2017-12-21 14:56:43 -0700

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,27 @@
-const autoprefixer = require('autoprefixer')
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
   siteMetadata: {
-    title: 'Gromsby Demo',
+    title: "Gromsby Demo"
   },
-  pathPrefix: '/gatsby-starter-grommet',
+  pathPrefix: "/gatsby-starter-grommet",
   plugins: [
     {
-      resolve: 'custom-sass-loader',
+      resolve: "custom-sass-loader",
       options: {
         postCssPlugins: [
           autoprefixer({
-            browsers: ['last 2 versions'],
+            browsers: ["last 2 versions"]
           })
         ]
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-111504724-1",
+        anonymize: true
+      }
     }
-  ],
-}
-
+  ]
+};
